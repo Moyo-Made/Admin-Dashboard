9eadb20b7f0aa37da08219b62b5a69016e9b337f
Moyomade <adegbitemoyomade2004@gmail.com>
2024-03-28 14:19:20 +0000

    Added data for sidebar menu

---
+++ src/sidebarMenu.js
@@ -0,0 +1,13 @@
+import Dashboard from "./assets/Fill 1 (2).png";
+import Claims from "./assets/Group 6.png";
+import Biller from "./assets/Group.png";
+import Path from "./assets/Path.png";
+import Health from "./assets/Health.png";
+
+export const menuItems = [
+	{ icon: Dashboard, alt: "Dashboard", label: "Dashboard" },
+	{ icon: Claims, alt: "Claims", label: "Claims" },
+	{ icon: Biller, alt: "Biller", label: "Biller Queue" },
+	{ icon: Path, alt: "Subscription", label: "Subscription" },
+	{ icon: Health, alt: "Health", label: "Health" },
+];
